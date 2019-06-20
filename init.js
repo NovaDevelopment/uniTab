0ccbb23f1ce8da1809a8782115891466661e42a2
Ben Zelnick <34197604+ZelnickB@users.noreply.github.com>
2019-06-20 18:58:58 -0400

    General Cleanups
    See diffs for details (too many to list here)

--- init.js
+++ init.js
@@ -1,7 +1,8 @@
 var clock = {testConsoleMessagesDisplayed:0, statusDisplayed:false};
-var user = {name:undefined, };
+var user = {name:undefined};
 function refreshclock() {
-    document.getElementById("clockText").innerHTML = moment().format('MMMM D, YYYY') + "<br>" + moment().format("H:mm")
+    document.getElementById("clockText").textContent = moment().format('MMMM D, YYYY');
+    document.getElementById("dateText").textContent = moment().format("H:mm");
     if(clock.testConsoleMessagesDisplayed <= 10){
         console.log("Clock refreshed on " + moment().format('YYYY.MM.DD') + " at " + moment().format("HH.mm.ss"))
         clock.testConsoleMessagesDisplayed ++;
@@ -15,6 +16,17 @@ if(getCookie("userName") == undefined) {
     document.getElementById("setup-name").removeAttribute("hidden");
 }
 else {
-    document.getElementById("greeting").innerHTML = "Hello, " + getCookie("userName") + ".";
+    document.getElementById("greeting").textContent = "Hello, " + getCookie("userName") + ".";
 }
 setInterval(refreshclock, 1000);
+document.getElementById("googleSearch").addEventListener("keydown", function() {
+    if(event.which === 13) {
+        gSearch(document.getElementById('googleSearch').value);
+    }
+}, false);
+document.getElementById("button-Search").addEventListener("click", function() {
+    gSearch(document.getElementById('button-Search').value);
+}, false);
+document.getElementById("button-SaveName").addEventListener("click", function() {
+    setup.saveName(document.getElementById('userName').value);
+}, false);
--- setup.js
+++ setup.js
@@ -1,6 +1,4 @@
-var setup = {
-    
-}
+var setup = {}
 setup.saveName = function(userName) {
     if(userName != null) {
         document.cookie = "userName=" + userName + "; expires=Fri, 31 Dec 9999 23:59:59 UTC";
